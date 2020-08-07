40a1a80d074fe86e052c8e42b89e4d9eee62bf99
Henrik Karlsson <henrik10@kth.se>
2020-08-07 15:00:21 +0200

    Updated traps.

--- src/include/traps.h
+++ src/include/traps.h
@@ -1,7 +1,10 @@
 #ifndef _TRAPS_H
 #define _TRAPS_H
-void trap_entry(void) __attribute__ ((noreturn));
-void trap_exit(pcb_t *pcb) __attribute__ ((noreturn));
-void handle_excpt(pcb_t *pcb, long mcause, long mtval) __attribute__ ((noreturn));
-void handle_intrp(pcb_t *pcb, long mcause, long mtval) __attribute__ ((noreturn));
+#include "pcb.h"
+#include "types.h"
+
+extern void trap_entry(void) __attribute__ ((noreturn));
+extern void trap_exit(pcb_t *pcb) __attribute__ ((noreturn));
+void handle_excpt(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) __attribute__ ((noreturn));
+void handle_intrp(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) __attribute__ ((noreturn));
 #endif /* _TRAPS_H */
