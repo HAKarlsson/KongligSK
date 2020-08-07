9f44a9030ec867de5d6c2c8ba4004a7d1745c26f
Henrik Karlsson <henrik10@kth.se>
2020-08-07 11:30:06 +0200

    API update.

--- konglig_api.h
+++ konglig_api.h
@@ -2,15 +2,24 @@
 #define _KONGLIG_API_H
 #define __ALWAYS_INLINE__ __attribute__ ((always_inline))
 
+/*** Base system calls ***/
 /* Yield to `zone`. */
-inline void ksk_Yield(int zone) __ALWAYS_INLINE__;
+inline void ksk_YieldTo(int zone) __ALWAYS_INLINE__;
+/* Atomically read and write to csr. */
+inline unsigned long ksk_CSRRW(int csr_num, long value) __ALWAYS_INLINE__;
+/* Atomically read and set to csr. */
+inline unsigned long ksk_CSRRS(int csr_num, long value) __ALWAYS_INLINE__;
+/* Atomically read and clear to csr. */
+inline unsigned long ksk_CSRRC(int csr_num, long value) __ALWAYS_INLINE__;
+
+/*** Pseudo system calls ***/
 /* Read csr. */
-inline unsigned long ksk_ReadCSR(int csr_num) __ALWAYS_INLINE__;
+inline unsigned long ksk_CSRR(int csr_num) __ALWAYS_INLINE__;
 /* Write csr. */
-inline unsigned long ksk_WriteCSR(int csr_num, long value) __ALWAYS_INLINE__;
-/* Clear csr. */
-inline unsigned long ksk_ClearCSR(int csr_num, long value) __ALWAYS_INLINE__;
+inline void ksk_CSRW(int csr_num, long value) __ALWAYS_INLINE__;
 /* Set csr. */
-inline unsigned long ksk_SetCSR(int csr_num, long value) __ALWAYS_INLINE__;
+inline void ksk_CSRS(int csr_num, long value) __ALWAYS_INLINE__;
+/* Clear csr. */
+inline void ksk_CSRC(int csr_num, long value) __ALWAYS_INLINE__;
 
 #endif /* _KONGLIG_API_H */
