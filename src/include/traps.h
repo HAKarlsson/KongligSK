57f894c55206af70b2a5978958a9b83f11c12912
Henrik Karlsson <henrik10@kth.se>
2020-08-08 23:56:31 +0200

    Added util.h for macros.

--- src/include/traps.h
+++ src/include/traps.h
@@ -2,9 +2,11 @@
 #define _TRAPS_H
 #include "pcb.h"
 #include "types.h"
+#include "util.h"
 
-extern void trap_entry(void) __attribute__ ((noreturn));
-extern void trap_exit(pcb_t *pcb) __attribute__ ((noreturn));
-void handle_excpt(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) __attribute__ ((noreturn));
-void handle_intrp(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) __attribute__ ((noreturn));
+extern void trap_entry(void) NO_RETURN;
+extern void trap_exit(pcb_t *pcb) NO_RETURN;
+void handle_excpt(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) NO_RETURN; 
+void handle_intrp(pcb_t *pcb, uintptr_t mcause, uintptr_t mtval) NO_RETURN;
+
 #endif /* _TRAPS_H */
---
+++ src/include/util.h
@@ -0,0 +1,9 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#define NO_RETURN __attribute__ ((noreturn))
+#define ALWAYS_INLINE __attribute__ ((always_inline))
+#define HOT __attribute__ ((cold))
+#define COLD __attribute__ ((cold))
+
+#endif /* _UTIL_H */
