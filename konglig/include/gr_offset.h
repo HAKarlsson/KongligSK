7b41875bf2ae9c68f2236f9376940ed7ea3cac1b
Henrik Karlsson <henrik10@kth.se>
2020-08-11 09:15:32 +0200

    Added GNU's required copyright headers.

--- konglig/include/gr_offset.h
+++ konglig/include/gr_offset.h
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of KongligSK.
+ * Copyright (c) 2020 Henrik Karlsson <henrik10@kth.se>.
+ * 
+ * KongligSK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * KongligSK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with KongligSK.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _GR_OFFSET_H
 #define _GR_OFFSET_H
 #include "util.h"
